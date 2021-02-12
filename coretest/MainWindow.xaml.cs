0cfaca8ab86ee4853658063a556b87a666f1a5b9
Jeff <jeff.yoder@testframework.io>
2021-02-12 13:45:49 -0700

    Add project files.

---
+++ coretest/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace coretest {
+
+   public partial class MainWindow: Window {
+      public MainWindow() {
+         InitializeComponent();
+      }
+
+      private void close_Click(object sender, RoutedEventArgs e) {
+         Application.Current.Shutdown();
+      }
+   }
+}
